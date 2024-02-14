73c4889153255b99847c4879a34df8ca918c16be
Wesley Ford <wes@wesdevpro.com>
2024-02-13 20:51:40 -0600

    fix: update harpoon shortcuts

--- lua/wesdevpro/plugins/harpoon.lua
+++ lua/wesdevpro/plugins/harpoon.lua
@@ -11,10 +11,10 @@ return {
             return {
                 {"<leader>a", mark.add_file},
                 {"<C-e>", ui.toggle_quick_menu},
-                {"<C-1>", function() ui.nav_file(1) end},
-                {"<C-2>", function() ui.nav_file(2) end},
-                {"<C-3>", function() ui.nav_file(3) end},
-                {"<C-4>", function() ui.nav_file(4) end},
+                {"<C-t>", function() ui.nav_file(1) end},
+                {"<C-h>", function() ui.nav_file(2) end},
+                {"<C-m>", function() ui.nav_file(3) end},
+                {"<C-n>", function() ui.nav_file(4) end},
             }
         end,
     },
